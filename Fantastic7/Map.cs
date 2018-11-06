f9e3d64f06c6ae0e513dee9b50e739cf6b0e791d
cmphillips234 <caleb@zsalon.biz>
2018-11-06 17:01:55 -0500

    Add files via upload
    This version includes all of the setup for the control system.
    
    The final control scheme is still in progress, but the ground-work is done.

---
+++ Fantastic7/Map.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Fantastic7
+{
+    class Map
+    {
+        private Room rm;
+        public void GenerateMap()
+        {
+
+        }
+
+
+    }
+}
